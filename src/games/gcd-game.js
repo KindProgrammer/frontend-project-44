906c082a40c75092f7fceb99804b6abccd95b66b
Daria Ignatieva <dariyignatieva@mail.ru>
2024-05-13 18:58:45 +0300

    Created a gcd game and created an executable file for it

---
+++ src/games/gcd-game.js
@@ -0,0 +1,25 @@
+import playGame from "../index.js"
+
+const getGcd = (n, m) => {
+    if (m !== 0) {
+      const k = n % m;
+      return getGcd(m, k);
+    }
+    return n;
+  };
+
+const makeGcdNumbers = () => {
+    const firstNumber = Math.floor(Math.random() * 100)
+    const secondNumber = Math.floor(Math.random() * 100)
+
+    const rightAnswer = getGcd(firstNumber, secondNumber)
+    const question = `${firstNumber} ${secondNumber}`
+
+    return [rightAnswer, question]
+}
+
+const rule = 'Find the greatest common divisor of given numbers.'
+
+export default () => {
+    playGame(makeGcdNumbers, rule)
+}
