7f15222d485127363c188d1e67b86f5b06702c7f
Daria Ignatieva <dariyignatieva@mail.ru>
2024-05-13 15:39:25 +0300

    Added index.js. There is general game logic

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+const getName = () => {
+    console.log('Welcome to the Brain Games!')
+    const name = readlineSync.question('May I have your name? ')
+    return name
+};
+
+const playGame = (questionAnswerFunc, rule) => {
+    const name = getName()
+
+    console.log(`Hello, ${name}!`)
+    console.log(rule)
+
+    let i = 0
+    while (i < 3) {
+        const questionAnswer = questionAnswerFunc()
+        const rightAnswer = questionAnswer[0]
+        const question = questionAnswer[1]
+
+        console.log(`Question: ${question}`)
+        const answer = readlineSync.question('Your answer: ').toLowerCase().trim()
+        if (answer !== rightAnswer) {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`)
+            console.log(`Let's try again, ${name}!`)
+            return
+        }
+        console.log('Correct!')
+        i ++
+    }
+
+    console.log(`Congratulations, ${name}!`)
+}
+
+export default playGame
